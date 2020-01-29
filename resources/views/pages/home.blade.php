82c77ce4ff4ea99abd90f0ef0feff05956844c85
phaniophrero <phaniophrero@yahoo.com>
2020-01-29 14:13:23 +0100

    Part 7- Contact

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,7 +40,7 @@
         <div class="container-contact">
             <h1 class="text-contact">Get in touch</h1>
             <p class="description-contact">Send us an email and tell us how can we help you. <br>
-                Talk to .
+                Talk to you soon.
             </p>
         </div>
         <div class="contact-container-right">
