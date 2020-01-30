efd1e1a1c821264e77bc16256dcf3fbf6af8e23b
phaniophrero <phaniophrero@yahoo.com>
2020-01-30 18:35:09 +0100

    Part 9 - test develop branch

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,7 +40,9 @@
         <div class="container-contact">
             <h1 class="text-contact">Get in touch</h1>
             <p class="description-contact">Send us an email and tell us how can we help you.<br>
-            Talk to you soon!</p>
+            Talk to you soon! <br>
+            We are here for you !
+            </p>
         </div>
         <div class="contact-container-right">
             <div class="contact-wrapper-right">
