e9bfdab402fbdda7fb29f0a4ec70f3683da6d161
phaniophrero <phaniophrero@yahoo.com>
2020-01-30 19:09:17 +0100

    Part 9 - update contact section test

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,7 +40,8 @@
         <div class="container-contact">
             <h1 class="text-contact">Get in touch</h1>
             <p class="description-contact">Send us an email and tell us how can we help you. <br>
-                Talk to you soon !
+                Talk to you soon ! <br>
+                We're here for you !
             </p>
         </div>
         <div class="contact-container-right">
