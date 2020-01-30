3b35675328c081bcdcb6783a9a964fa8668c3e76
phaniophrero <phaniophrero@yahoo.com>
2020-01-30 19:19:35 +0100

    Part 9 - change back contact section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,8 +40,7 @@
         <div class="container-contact">
             <h1 class="text-contact">Get in touch</h1>
             <p class="description-contact">Send us an email and tell us how can we help you. <br>
-                Talk to you soon ! <br>
-                We're here for you !
+                Talk to you soon !
             </p>
         </div>
         <div class="contact-container-right">
